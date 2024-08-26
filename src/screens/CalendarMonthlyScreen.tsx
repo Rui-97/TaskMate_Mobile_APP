455510c015fc21baa182f6af326b03fe0d659d47
Cindy <cindy.rui.wei@gmail.com>
2024-08-25 21:04:38 -0400

    impl week view calendar

--- src/screens/CalendarMonthlyScreen.tsx
+++ src/screens/CalendarMonthlyScreen.tsx
@@ -1,17 +1,22 @@
-import { View, SafeAreaView, Text } from "react-native";
-import { CalendarList, CalendarUtils, DateData } from "react-native-calendars";
+import { SafeAreaView } from "react-native";
+import { CalendarList, CalendarUtils } from "react-native-calendars";
 import { Dimensions } from "react-native";
 import { useState } from "react";
 
 import MonthlyViewDateItem from "../components/Calendar/MonthlyViewDateItem";
+import SelectViewIconButton from "../components/Calendar/SelectViewIconButton";
+import SelectViewModal from "../components/Calendar/SelectViewModal";
 
 const CalendarMonthlyScreen = () => {
   const initialDate = CalendarUtils.getCalendarDateString(new Date());
   const [selectedDate, setSelectedDate] = useState(initialDate);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const screenHeight = Dimensions.get("window").height;
 
   return (
     <SafeAreaView>
+      <SelectViewIconButton onPress={setIsModalVisible} />
       <CalendarList
         dayComponent={({ date, state }) => (
           <MonthlyViewDateItem
@@ -22,6 +27,13 @@ const CalendarMonthlyScreen = () => {
           />
         )}
         calendarHeight={(screenHeight / 7) * 6}
+        theme={{
+          calendarBackground: "#f1f1f1",
+        }}
+      />
+      <SelectViewModal
+        isVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
       />
     </SafeAreaView>
   );
--- src/screens/CalendarWeeklyScreen.tsx
+++ src/screens/CalendarWeeklyScreen.tsx
@@ -1,11 +1,87 @@
-import { View, SafeAreaView, Text } from "react-native";
+import { View, SafeAreaView, Text, StyleSheet } from "react-native";
+import {
+  ExpandableCalendar,
+  TimelineEventProps,
+  TimelineList,
+  CalendarProvider,
+  WeekCalendar,
+  TimelineProps,
+  CalendarUtils,
+} from "react-native-calendars";
+import { groupBy } from "lodash";
+
+import { formatDate } from "../utils/utils";
+
+const INITIAL_TIME = { hour: 9, minutes: 0 };
+
+const events = [
+  {
+    id: "1",
+    date: "2024-08-25",
+    start: "2024-08-25 09:00:00",
+    end: "2024-08-25 10:00:00",
+    title: "Morning Meeting",
+    color: "blue",
+  },
+  {
+    id: "2",
+    date: "2024-08-25",
+    start: "2024-08-25 11:00:00",
+    end: "2024-08-25 12:00:00",
+    title: "Team Standup",
+    color: "green",
+  },
+  {
+    id: "3",
+    date: "2024-08-26",
+    start: "2024-08-26 14:00:00",
+    end: "2024-08-26 15:00:00",
+    title: "Client Call",
+    color: "red",
+  },
+];
 
 const CalendarWeeklyScreen = () => {
+  const eventsByDate = groupBy(events, (e) =>
+    CalendarUtils.getCalendarDateString(e.start)
+  );
+  console.log(eventsByDate);
   return (
-    <SafeAreaView>
-      <Text>CalendarWeekly Screen</Text>
+    <SafeAreaView style={{ width: "100%", height: "100%" }}>
+      <CalendarProvider
+        date={formatDate(new Date())}
+        showTodayButton
+        disabledOpacity={0.6}
+      >
+        <ExpandableCalendar
+          firstDay={1}
+          theme={{
+            calendarBackground: "#f1f1f1",
+            selectedDayBackgroundColor: "#687dcc",
+            arrowColor: "#687dcc",
+          }}
+        />
+        <View style={styles.container}>
+          <TimelineList
+            events={eventsByDate}
+            initialTime={INITIAL_TIME}
+            timelineProps={{
+              format24h: true,
+            }}
+            showNowIndicator
+            scrollToFirst
+          />
+        </View>
+      </CalendarProvider>
     </SafeAreaView>
   );
 };
 
 export default CalendarWeeklyScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+});
